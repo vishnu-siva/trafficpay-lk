69977b9d322850a2d6f6c7a79bd71e60b6312b51
Thurga 1125 <thurga11252001@gmail.com>
2026-05-09 18:43:05 +0530

    Remove all files

--- sl-traffic-fine-system/backend/src/main/java/lk/gov/police/trafficfine/exception/AlreadyPaidException.java
+++
@@ -1,7 +0,0 @@
-package lk.gov.police.trafficfine.exception;
-
-public class AlreadyPaidException extends RuntimeException {
-    public AlreadyPaidException(String message) {
-        super(message);
-    }
-}
--- sl-traffic-fine-system/backend/src/main/java/lk/gov/police/trafficfine/exception/FineNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package lk.gov.police.trafficfine.exception;
-
-public class FineNotFoundException extends RuntimeException {
-    public FineNotFoundException(String message) {
-        super(message);
-    }
-}
